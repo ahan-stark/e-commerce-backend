637d959595e455e1ef0077fce62d723dd55181ca
ahan-stark <ahanabubakar@gmail.com>
2022-11-13 21:37:17 +0530

    added lombok Dependency

--- src/main/java/com/example/ecommerce/Entities/Category.java
+++ src/main/java/com/example/ecommerce/Entities/Category.java
@@ -1,40 +1,22 @@
 package com.example.ecommerce.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 @Entity(name = "category")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Category {
     @Id
     Integer categoryId;
     String categoryName;
-
-    public Category(Integer categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-    }
-
-    public Integer getcategoryId() {
-        return categoryId;
-    }
-
-    public void setcategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getcategoryName() {
-        return categoryName;
-    }
-
-    public void setcategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
     @Override
     public String toString() {
         return "Category{" +
--- src/main/java/com/example/ecommerce/Entities/Products.java
+++ src/main/java/com/example/ecommerce/Entities/Products.java
@@ -1,60 +1,24 @@
 package com.example.ecommerce.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Table
 @Entity(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Products {
     @Id
     Integer productId;
     String productName;
     Integer productCategoryId;
     String productImage;
-
-    public Products(Integer productId, String productName, Integer productCategoryId, String productImage) {
-        this.productId = productId;
-        this.productName = productName;
-        this.productCategoryId = productCategoryId;
-        this.productImage = productImage;
-    }
-
-    public Products() {
-    }
-
-    public Integer getproductId() {
-        return productId;
-    }
-
-    public void setproductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public String getproductName() {
-        return productName;
-    }
-
-    public void setproductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Integer getproductCategoryId() {
-        return productCategoryId;
-    }
-
-    public void setproductCategoryId(Integer productCategoryId) {
-        this.productCategoryId = productCategoryId;
-    }
-
-    public String getproductImage() {
-        return productImage;
-    }
-
-    public void setproductImage(String productImage) {
-        this.productImage = productImage;
-    }
-
     @Override
     public String toString() {
         return "Products{" +
