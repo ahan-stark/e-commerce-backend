1d1b0ef509d074f78280645b8ca82026843a5b2d
ahan-stark <ahanabubakar@gmail.com>
2023-03-03 23:47:18 +0530

    added scheduling to check for every 30 mins

--- src/main/java/com/example/ecommerce/controller/SuperCart.java
+++ src/main/java/com/example/ecommerce/controller/SuperCart.java
@@ -2,10 +2,8 @@ package com.example.ecommerce.controller;
 
 import com.example.ecommerce.dto.NotifyReturn;
 import com.example.ecommerce.dto.SuperCartReturn;
-import com.example.ecommerce.dto.UserInformation;
 import com.example.ecommerce.entities.User;
 import com.example.ecommerce.service.SuperCartService;
-import com.example.ecommerce.service.TwilioService;
 import com.example.ecommerce.service.UserInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +15,6 @@ import java.util.List;
 public class SuperCart {
     @Autowired
     SuperCartService superCartService;
-    @Autowired
-    UserInformationService userInformationService;
-    @Autowired
-    TwilioService twilioService;
-
     @PostMapping("/super-cart/{userId}/{productId}/{productBookingPrice}")
     public void addToSuperCart(@PathVariable("userId") Integer userId, @PathVariable("productId") Integer productId, @PathVariable("productBookingPrice") Integer productBookingPrice) {
         superCartService.addToSuperCart(userId, productId, productBookingPrice);
@@ -40,33 +33,6 @@ public class SuperCart {
     @GetMapping("/check-superCart/{userId}/{productId}")
     public SuperCartReturn checkSuperCart(@PathVariable("userId") Integer userId, @PathVariable("productId") Integer productId) {
         return superCartService.checkSuperCart(userId, productId);
-    }
-
-    @GetMapping("notify-user")
-    public void getItemsToNotify() {
-        List<NotifyReturn> val = superCartService.getItemsToNotify();
-        for (NotifyReturn a : val) {
-            if (a.getProductBookingPrice() > a.getProductPrice()) {
-                User user = userInformationService.getUserDetails(a.getUserId());
-                if (a.getBookingStatus().equals("notify")) {
-                    System.out.println("notify");
-                    superCartService.updateSuperCartStatus(a.getUserId(), a.getProductId());
-                    //                twilioService.sendSms(String.valueOf(userInformation.getUserPhoneNo()),"your desired product"+ a.getProductName()+"is below "+ a.getProductBookingPrice()+" buy it now!");
-                } else if (a.getBookingStatus().equals("notified")) {
-                    System.out.println("buy it");
-                    bookFromSuperCart(a.getUserId(), a.getProductId());
-                    deleteFromSuperCartAfterCheckOut(a.getUserId(), a.getProductId());
-                }
-            }
-        }
-    }
-
-    public void bookFromSuperCart(Long userId, Integer productId) {
-        superCartService.bookFromSuperCart(userId, productId);
-    }
-
-    public void deleteFromSuperCartAfterCheckOut(Long userId, Integer productId) {
-        superCartService.deleteSuperCart(userId, productId);
     }
 
 }
---
+++ src/main/java/com/example/ecommerce/feature/Notify.java
@@ -0,0 +1,49 @@
+package com.example.ecommerce.feature;
+
+import com.example.ecommerce.dto.NotifyReturn;
+import com.example.ecommerce.entities.User;
+import com.example.ecommerce.service.SuperCartService;
+import com.example.ecommerce.service.TwilioService;
+import com.example.ecommerce.service.UserInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@EnableScheduling
+public class Notify {
+    @Autowired
+    SuperCartService superCartService;
+    @Autowired
+    UserInformationService userInformationService;
+    @Autowired
+    TwilioService twilioService;
+    @Scheduled(fixedRate = 10000)
+    public void getItemsToNotify() {
+        List<NotifyReturn> val = superCartService.getItemsToNotify();
+        for (NotifyReturn a : val) {
+            if (a.getProductBookingPrice() > a.getProductPrice()) {
+                User user = userInformationService.getUserDetails(a.getUserId());
+                if (a.getBookingStatus().equals("notify")) {
+                    System.out.println("notify");
+                    superCartService.updateSuperCartStatus(a.getUserId(), a.getProductId());
+                    //                twilioService.sendSms(String.valueOf(userInformation.getUserPhoneNo()),"your desired product"+ a.getProductName()+"is below "+ a.getProductBookingPrice()+" buy it now!");
+                } else if (a.getBookingStatus().equals("notified")) {
+                    System.out.println("buy it");
+                    bookFromSuperCart(a.getUserId(), a.getProductId());
+                    deleteFromSuperCartAfterCheckOut(a.getUserId(), a.getProductId());
+                }
+            }
+        }
+    }
+    public void bookFromSuperCart(Long userId, Integer productId) {
+        superCartService.bookFromSuperCart(userId, productId);
+    }
+
+    public void deleteFromSuperCartAfterCheckOut(Long userId, Integer productId) {
+        superCartService.deleteSuperCart(userId, productId);
+    }
+}
